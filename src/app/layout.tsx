b39843c3b290132b4735bfc91370f5293d7e147c
Marc <meillac.marc.pro@gmail.com>
2023-09-14 09:01:32 +0200

    build: add eslint and prettier rules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import 'server-only';
 
-const inter = Inter({ subsets: ["latin"] });
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Simmerize",
-  description: "A collection of delicious recipes!",
+  title: 'Simmerize',
+  description: 'A collection of delicious recipes!',
 };
 
 export default function RootLayout({
