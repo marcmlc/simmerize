fbfdb5fc08480129d3bc620dc5a36b08e49ef679
Marc <meillac.marc.pro@gmail.com>
2023-10-01 14:41:14 +0200

    feat(recipes): add recipe meta items

--- src/components/Recipe/Recipe.tsx
+++ src/components/Recipe/Recipe.tsx
@@ -1,10 +1,11 @@
 import 'server-only';
 
-import { grid, gridItem } from '@styles/patterns';
+import { grid, gridItem, vstack } from '@styles/patterns';
 
 import { RecipeDirections } from './RecipeDirections';
 import { RecipeImage } from './RecipeImage';
 import { RecipeIngredients } from './RecipeIngredients';
+import { RecipeMeta } from './RecipeMeta';
 import { RecipeName } from './RecipeName';
 import { RecipeTags } from './RecipeTags';
 import { Recipe } from '@/types';
@@ -20,39 +21,43 @@ export function Recipe({ recipe }: Props) {
         columns: 12,
         rowGap: { base: 6, lg: 12 },
         columnGap: { base: 0, lg: 20 },
-      })}
-    >
+      })}>
       <div
         className={gridItem({
           colSpan: { base: 12, lg: 7 },
           order: { lg: 2 },
-        })}
-      >
-        <RecipeName name={recipe.name} />
-        <RecipeTags tags={recipe.tags} />
+        })}>
+        <div
+          className={vstack({
+            alignItems: 'flex-start',
+            gap: { base: 6, lg: 10 },
+          })}>
+          <div>
+            <RecipeName name={recipe.name} />
+            <RecipeTags tags={recipe.tags} />
+          </div>
+          <RecipeMeta cookingTime={recipe.cookingTime} preparationTime={recipe.preparationTime} />
+        </div>
       </div>
       <div
         className={gridItem({
           colSpan: { base: 12, lg: 5 },
           order: { lg: 1 },
-        })}
-      >
+        })}>
         <RecipeImage image={recipe.image} slug={recipe.slug} />
       </div>
       <div
         className={gridItem({
           colSpan: { base: 12, lg: 5 },
           order: { lg: 3 },
-        })}
-      >
+        })}>
         <RecipeIngredients ingredients={recipe.ingredients} />
       </div>
       <div
         className={gridItem({
           colSpan: { base: 12, lg: 7 },
           order: { lg: 4 },
-        })}
-      >
+        })}>
         <RecipeDirections directions={recipe.directions} />
       </div>
     </div>
---
+++ src/components/Recipe/RecipeMeta.tsx
@@ -0,0 +1,56 @@
+import { css, cx } from '@styles/css';
+import { grid, gridItem } from '@styles/patterns';
+
+import { getTotalTime } from '@/utils/time';
+
+type Props = {
+  preparationTime: number;
+  cookingTime: number;
+};
+
+const PLACEHOLDER = '--';
+
+export function RecipeMeta({ preparationTime, cookingTime }: Props) {
+  const totalTime = getTotalTime({ preparationTime, cookingTime });
+
+  return (
+    <div
+      className={grid({
+        columns: 12,
+        width: 'full',
+        rounded: 'md',
+        paddingBlock: '2.5',
+        border: '1px solid token(colors.gray.200)',
+      })}>
+      <RecipeMetaItem
+        className={gridItem({ colSpan: { base: 6, sm: 4 } })}
+        time={preparationTime}
+        label="Préparation"
+      />
+      <RecipeMetaItem className={gridItem({ colSpan: { base: 6, sm: 4 } })} time={cookingTime} label="Cuisson" />
+      <RecipeMetaItem className={gridItem({ colSpan: { base: 12, sm: 4 } })} time={totalTime} label="Total" />
+    </div>
+  );
+}
+
+function RecipeMetaItem({ time, label, className }: { time: number; label: string; className?: string }) {
+  return (
+    <div
+      className={cx(
+        className,
+        css({
+          alignItems: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+        })
+      )}>
+      <p className={css({ fontSize: { base: 'sm', sm: 'md' }, fontWeight: 600, textTransform: 'uppercase' })}>
+        {label}
+      </p>
+      <p>
+        <span className={css({ fontWeight: 500, fontSize: 'lg' })}>{time || PLACEHOLDER}</span>&nbsp;
+        {time && <span className={css({ fontSize: 'sm' })}>minutes</span>}
+      </p>
+    </div>
+  );
+}
